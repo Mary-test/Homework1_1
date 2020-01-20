9e94b69eba663e698b58c075ddcb832e5496bf09
Mary-test <57749974+Mary-test@users.noreply.github.com>
2020-01-20 13:06:59 +0400

    Add files via upload

---
+++ Homework_5.py
@@ -0,0 +1,23 @@
+class Student:
+    def __init__(self, name, surname, age, gender, institution, faculty, course, admissionnumber, marks):
+        self.name = name
+        self.surname = surname
+        self.age = age
+        self.gender = gender
+        self.institution = institution
+        self.faculty = factulty
+        self.course = course
+        self.admissionnumber = admissionnumber
+        self.gotmarks = gotmarks
+
+
+def selected_subscribtion(self):
+    def selected_professors(self):
+      def passed_seminars(self):
+        def passed_exams(self):
+          def eligible_to_end_the_institution(self):
+
+student1 = Student("Mary","Smith",17,"female","University1","Mathematics","first", 846488,100)
+
+print(student1.name)
+student1.passed_exams()
