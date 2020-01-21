03f80d73c00f6a4988b3cf24fbb9e96ff5e79d58
Mary-test <57749974+Mary-test@users.noreply.github.com>
2020-01-21 11:00:22 +0400

    Add files via upload

--- Homework_5.py
+++ Homework_5.py
@@ -5,19 +5,16 @@ class Student:
         self.age = age
         self.gender = gender
         self.institution = institution
-        self.faculty = factulty
+        self.faculty = faculty
         self.course = course
         self.admissionnumber = admissionnumber
-        self.gotmarks = marks
-
-
-def selected_subscribtion(self):
-    def selected_professors(self):
-      def passed_seminars(self):
-        def passed_exams(self):
-          def eligible_to_end_the_institution(self):
-
-student1 = Student("Mary","Smith",17,"female","University1","Mathematics","first", 846488,100)
-
-print(student1.name)
-student1.passed_exams()
+        self.marks = marks
+        def select_subscribtion(arg):
+    print(arg.name, "has selected new subscription")
+    def select_professor(arg):
+        print(arg.name, "has selected Pr.Jones")
+        def pass_exam(arg):
+            print(arg.name, "has passed exams")
+            student1 = Student("Mary", "Smith", 17, "female", "University1", "Mathematics", "first", 846488, 100)print(student1.name)
+print(student1.admissionnumber)
+student1.select_subscribtion()
